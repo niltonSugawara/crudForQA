9df7f59119c010950baa2282c82ba7a5e63c50b4
Nilton Mitsuharu Sugawara <nilton.sugawara@gmail.com>
2021-03-10 08:12:27 -0300

    commit inicial

---
+++ src/main/java/com/br/nimisu/crudforqa/CrudforqaApplication.java
@@ -0,0 +1,13 @@
+package com.br.nimisu.crudforqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudforqaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudforqaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/nimisu/crudforqa/CrudforqaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.nimisu.crudforqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudforqaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
